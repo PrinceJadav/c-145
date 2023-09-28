eb1f4d53ffc5b6715e1d4dd3fa39005fffde0a48
PrinceJadav <95040464+PrinceJadav@users.noreply.github.com>
2023-09-28 15:26:11 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 15:09:55 2023
+
+@author: User
+"""
+
+a=100
+print(type(a))
+
+<class 'int'>
+
+city ='Pune'
+print(type(city))
+
+<class 'str'>
+
+super_heroes = ["Ironman","Spiderman","CaptainAmerica"];
+print(type(super_heroes))
---
+++ untitled6.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 15:17:51 2023
+
+@author: User
+"""
+from tiknter import =
+root= Tk()
+
+root.title("Addition")
+root.geometry("400x200")
+
+
+root.mainloop()
+
+show_results=label(root)
+
+def add():
+   result=4+1
+   show_result["text"]=res
+   
+   btn = Button(root, text="Add" command=add,)
+   btn.pack()
+   
+   show_results.pack()
+   
+   
+   root.mainloop
+   
